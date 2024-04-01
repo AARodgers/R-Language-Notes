3febaa4fc8fc6eb66c6d66baf6f67d540ad53124
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-01 15:30:33 -0600

    Add vector notes

--- r_notes.R
+++ r_notes.R
@@ -1,3 +1,4 @@
+# START
 # connecting to SQL DB (ODBC is Open DB Connectivity)
 
 library(DBI)
@@ -15,3 +16,26 @@ data <- dbGetQuery( con, "SELECT * FROM mytable")
 
 #close the database connection
 dbDisconnect(con)
+
+#END ========================
+
+# START
+# Reading and writing to a CSV file
+data <- read.csv("file.csv")
+write.csv(data, "file.csv")
+# END ======================
+
+#START
+# Reading and writing an Excel File
+install.packages("readx1")
+library(readx1)
+data <- read_excel("file.xlsx")
+write.xlsx(data, "file.xlsx", sheetName = "Sheet1", colNames = TRUE,
+           rowNames = FALSE)
+# END
+
+#START
+# vector is like a list of things all with same data type
+nums <- c(1, 2, 3, 4)
+chars <- c("red", "green", "blue")
+#END
