c4b3ec49d6d6ed2de65d56beee7aed53ed75514e
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-02 13:06:00 -0600

    Filter by height and mass

--- starwars.R
+++ starwars.R
@@ -7,4 +7,19 @@ library(tidyverse)
 View(starwars)
 ?starwars
 
+# Get height and mass data and view in a plot
+starwars %>%
+  #filter is by the rows or observations
+  filter(height > 150 & mass < 200) %>%
+  #change the height from cm to meters
+  mutate(height_in_meters = height/100) %>%
+  #select the height_in_meters column and mass column to work with
+  select(height_in_meters, mass) %>%
+  # sort by mass smallest to largest
+  arrange(mass) %>%
+  # view the table
+  #View()
+  # put into a scatter plot
+  plot()
 
+  
