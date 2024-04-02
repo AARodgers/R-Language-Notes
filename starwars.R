1c81d5af997071b7b8cf1e4644748b50e946e9bf
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-02 12:35:15 -0600

    Call on tidyverse and dataset

--- starwars.R
+++ starwars.R
@@ -1,8 +1,10 @@
-
-
 #call on tidyverse packages
 library(tidyverse)
 
-# View the starwars data frame
-View(CO2)
-?CO2
+# View the data frame from original data, from SWAPI, the Star Wars API,
+#https://swapi.py4e.com/, has been revised to reflect additional research into
+#gender and sex determinations of characters.
+View(starwars)
+?starwars
+
+
