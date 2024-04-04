6184ade138e7b8cad46f15a956c395646c52d82f
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-03 19:33:04 -0600

    Add tidydata notes

--- notes.R
+++ notes.R
@@ -17,3 +17,20 @@
 # R is a 1 indexing language
 # sometimes you have to call garbage
 # you don't need to put return in R, it will return the last line of code
+
+Lesson 4.3.24
+The core principles of tidy data are:
+
+    Each Variable Forms a Column: In tidy data, each variable
+    (i.e., a characteristic or attribute being measured) is stored in its
+    own column. This means that each column represents a single variable, and
+    each value within that column corresponds to a specific observation.
+
+    Each Observation Forms a Row: Tidy data organizes data such that each
+    row represents a unique observation or case. This ensures that all
+    information pertaining to a single observation is contained within that row.
+
+    Each Type of Observational Unit Forms a Table: Tidy data is typically
+    organized into separate tables (data frames in R) for each type of observational unit. For example, if a dataset contains information about both individuals and their corresponding measurements, there would be one table for individuals and another for measurements, with a common key linking the two tables.
+
+    Each Value Should Be in One Cell: Tidy data ensures that each cell in the dataset contains a single value. This makes it easier to perform computations, filtering, and other operations on the data.
